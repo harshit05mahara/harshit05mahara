d9bb522d1f69ad40998adb27a88d4e46486ab148
harshit05mahara <158496595+harshit05mahara@users.noreply.github.com>
2024-08-09 07:37:46 +0530

    Add files via upload
    you can show to creativity in this project

---
+++ project 2.py
@@ -0,0 +1,46 @@
+# print('\n\n\n')
+# print( '\t\t\thiee bh***d kaun banega crorepati main aapka swagat hai')
+# print('\n\n')
+# c=input()
+ 
+que=['1.which language frequently used in japan?', 'japanese','punjabi','hindi','english']
+i=0
+money=0
+for i in range(0,1):
+    print(que[i])
+    print('\t\t\t\tpress any key to continue...')
+    b=input()
+    print('a.',que[1],'\t\t\t','b.',que[2],'\t\t\t','\n','c.',que[3],'\t\t\t','d.',que[4])
+    choose=int(input('enter your choice(1-4)\n'))
+    if (choose==1):
+        print("you are right")
+        money=1000
+        print('your winning price is :',money)
+    else:
+        print("better luck next time")
+        money=0
+        print('your Balance is :',money)
+
+print('\t\t\t-x-x-x-x-x-x-x-x-x')
+
+print("\t\t\t\tMoving to the next question....")
+
+
+
+que1=['2.which language frequently used in india?', 'japanese','punjabi','hindi','english ']
+i=0
+money1=0
+for i in range(0,1):
+    print(que1[i])
+    print('\t\t\t\tpress any key to continue...')
+    b=input()
+    print('a.',que1[1],'\t\t\t','b.',que1[2],'\t\t\t','\n','c.',que1[3],'\t\t\t','d.',que1[4])
+    choose=int(input('enter your choice(1-4)\n'))
+    if (choose==3):
+        print("you are right")
+        money1=1000+money
+        print('your winning price is :',money1)
+    else:
+        print("better luck next time")
+        money1=0+money
+        print('your Balance is :',money1)
